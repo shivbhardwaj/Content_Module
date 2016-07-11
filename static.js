162012aa798d46117dfbea99b3a803d3c93a9146
shivbhardwaj <sbhardwaj192@gmail.com>
2016-07-11 11:11:27 -0700

    all files for the content_module Node assignment.

---
+++ static.js
@@ -0,0 +1,88 @@
+var http = require('http');
+var fs = require('fs');
+// creating a server using http module:
+module.exports = function(request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/') {
+        fs.readFile('views/index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/cars") {
+         fs.readFile('views/cars.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    else if (request.url === "/cats") {
+         fs.readFile('views/cats.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    else if (request.url === "/cars/new") {
+         fs.readFile('views/cars_new.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    else if(request.url === '/images/cars1.jpg'){
+    // notice we won't include the utf8 encoding
+        fs.readFile('./images/cars1.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/cars2.jpg'){
+    // notice we won't include the utf8 encoding
+        fs.readFile('./images/cars2.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/cars3.jpg'){
+    // notice we won't include the utf8 encoding
+        fs.readFile('./images/cars3.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/cats1.jpg'){
+    // notice we won't include the utf8 encoding
+        fs.readFile('./images/cats1.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/cats2.jpg'){
+    // notice we won't include the utf8 encoding
+        fs.readFile('./images/cats2.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/cats3.jpg'){
+    // notice we won't include the utf8 encoding
+        fs.readFile('./images/cats3.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    // request didn't match anything:
+    else {
+        response.end('The requested URL is not available.');
+    }
+};
